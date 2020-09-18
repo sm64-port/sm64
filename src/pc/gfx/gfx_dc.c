2ebfae0bb92c52282277d47a8695d76e43b2f41e
HaydenKow <819028+mrneo240@users.noreply.github.com>
2020-09-17 21:37:50 -0400

    feat: introduce frame limiting and frame skipping
    - from mkst (3ds)

--- src/pc/gfx/gfx_dc.c
+++ src/pc/gfx/gfx_dc.c
@@ -14,27 +14,14 @@ static int force_30fps = 1;
 static unsigned int last_time = 0;
 
 extern void glKosSwapBuffers(void);
-
-/* Minimalist PSP SDK 0.15.0 for Windows whines about missing this, linux built toolchain doesn't care
- */
-char *stpcpy(char *__restrict__ dest, const char *__restrict__ src) {
-    while ((*dest++ = *src++) != '\0')
-        /* nothing */;
-    return --dest;
-}
-
-int isspace(int _c) {
-    char c = (char) _c;
-    return c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r';
-}
-
 extern uint64_t timer_ms_gettime64(void);
+extern void timer_spin_sleep(unsigned int ms);
+
 unsigned int GetSystemTimeLow(void) {
     uint64_t msec = timer_ms_gettime64();
     return (unsigned int) msec;
 }
 
-extern void timer_spin_sleep(unsigned int ms);
 void DelayThread(unsigned int ms) {
     timer_spin_sleep(ms);
 }
@@ -44,8 +31,7 @@ static void gfx_dc_init(UNUSED const char *game_name, UNUSED bool start_in_fulls
     last_time = GetSystemTimeLow();
 }
 
-static void
-gfx_dc_set_fullscreen_changed_callback(UNUSED void (*on_fullscreen_changed)(bool is_now_fullscreen)) {
+static void gfx_dc_set_fullscreen_changed_callback(UNUSED void (*on_fullscreen_changed)(bool is_now_fullscreen)) {
 }
 
 static void gfx_dc_set_fullscreen(UNUSED bool enable) {
@@ -73,25 +59,38 @@ static void gfx_dc_handle_events(void) {
     // DelayThread(100);
 }
 
+float cpu_time = 0.f, gpu_time = 0.f;
+uint8_t skip_debounce = 0;
+const unsigned int FRAME_TIME_MS = 34; // 33.3_
+
 static bool gfx_dc_start_frame(void) {
+    const unsigned int cur_time = GetSystemTimeLow();
+    const unsigned int elapsed = cur_time - last_time;
+
+    if (skip_debounce) {
+        skip_debounce--;
+        return true;
+    }
+    // skip if frame took longer than 1 / 30 = 33.3 ms
+    if (elapsed > FRAME_TIME_MS) {
+        skip_debounce = 3; // skip a max of once every 4 frames
+        last_time = cur_time;
+        return false;
+    }
     return true;
 }
 
 static void gfx_dc_swap_buffers_begin(void) {
     // Number of microseconds a frame should take (30 fps)
-    const unsigned int FRAME_TIME_US = 33333;
     const unsigned int cur_time = GetSystemTimeLow();
     const unsigned int elapsed = cur_time - last_time;
     last_time = cur_time;
 
-    if (force_30fps) {
-        if (elapsed < FRAME_TIME_US) {
+    if (force_30fps && elapsed < FRAME_TIME_MS) {
 #ifdef DEBUG
-            printf("elapsed %d us fps %f\n", elapsed, (1000.0f * 1000.0f) / elapsed);
+        printf("elapsed %d ms fps %f cur: %d\n", elapsed, 1000.0f / elapsed, (unsigned int)timer_ms_gettime64());
 #endif
-            DelayThread((FRAME_TIME_US - elapsed) / 1000);
-            last_time = cur_time + (FRAME_TIME_US - elapsed);
-        }
+        DelayThread(FRAME_TIME_MS - elapsed);
     }
 }
 
